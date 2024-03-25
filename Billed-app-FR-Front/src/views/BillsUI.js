stelda/bill-app

commit 08e1f207931a8650a100a0f18a2213cc8662be9f

Fix bug - Bills displayed in descending order

Billed-app-FR-Front/src/views/BillsUI.js
@@ -47,7 +47,10 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
+  // Fix Bug #1 - Sort bills by date
+  bills.sort((a, b) => new Date(b.date) - new Date(a.date));
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}

commit 0a5960c0345a79ff6b75b0bb328ded59cd66684a

Fix bug that prevents to launch the tests suite
the bug was introduced by commit 08e1f207931a8650a100a0f18a2213cc8662be9f

Billed-app-FR-Front/src/views/BillsUI.js
@@ -49,7 +49,9 @@ export default ({ data: bills, loading, error }) => {
   }
 
   // Fix Bug #1 - Sort bills by date
+  if (Array.isArray(bills)) {
   bills.sort((a, b) => new Date(b.date) - new Date(a.date));
+  }
 
   return (`
     <div class='layout'>