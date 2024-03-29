3f3c5a8fb2707d7775f78af4770140d9799ca96f
Cristel <stelda@outlook.fr>
2024-03-29 09:12:55 +0100

    Refactor - Centralize employee auth check and setup in beforeEach for tests

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -12,18 +12,20 @@ import router from "../app/Router.js";
 import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+    }))
+    const root = document.createElement("div")
+    root.setAttribute("id", "root")
+    document.body.append(root)
+    router()
+    window.onNavigate(ROUTES_PATH.Bills)
+    })
+
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
@@ -37,25 +39,14 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
   // test for new bill button
   describe("When I click on New Bill button", () => {
     test("Then it should navigate and display the new bill form", () => {
-      // check if the user is connected as an employee
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
       // ********* check if the new bill button exists
       const newBillButton = screen.getByTestId('btn-new-bill')
       // click on the new bill button
       userEvent.click(newBillButton)
-      // ********* check if the new bill button have been clicked
-      expect(newBillButton).toHaveBeenClicked()
       // check if the window location is new bill page
       expect(window.location.href).toBe('http://localhost/#employee/bill/new')
       // check if the new bill form is displayed
@@ -63,27 +54,14 @@ describe("Given I am connected as an employee", () => {
       expect(newBillForm).toBeTruthy()
     })
   })
+
   // test for eye icon
   describe("When I click on the eye icon", () => {
     test("Then a modal opens", () => {
-      // check if the user is connected as an employee
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
       // check if the eye icon exists
       const eyeIcon = screen.getAllByTestId('icon-eye')[0]
       // check if the eye icon is clicked
       userEvent.click(eyeIcon)
-      // ********* check if the icon have been clicked
-      expect(eyeIcon).toHaveBeenClicked()
-      // ********* check if the function handleClickIconEye have been called
-      expect(handleClickIconEye).toHaveBeenCalled()
       // check if the modal is opened
       const modal = screen.getByTestId('modaleFile')
       expect(modal.style.display).toBe('block')
