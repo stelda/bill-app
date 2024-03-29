0a01b280de88b9637c43e70c32a6e01fd82f16c4
Cristel <stelda@outlook.fr>
2024-03-29 08:43:24 +0100

    Add test - New bill button click and eye icon click

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -9,6 +9,7 @@ import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -36,4 +37,61 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+  // test for new bill button
+  describe("When I click on New Bill button", () => {
+    test("Then it should navigate and display the new bill form", () => {
+      // check if the user is connected as an employee
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      // ********* check if the new bill button exists
+      const newBillButton = screen.getByTestId('btn-new-bill')
+      // click on the new bill button
+      userEvent.click(newBillButton)
+      // ********* check if the new bill button have been clicked
+      expect(newBillButton).toHaveBeenClicked()
+      // check if the window location is new bill page
+      expect(window.location.href).toBe('http://localhost/#employee/bill/new')
+      // check if the new bill form is displayed
+      const newBillForm = screen.getByTestId('form-new-bill')
+      expect(newBillForm).toBeTruthy()
+    })
+  })
+  // test for eye icon
+  describe("When I click on the eye icon", () => {
+    test("Then a modal opens", () => {
+      // check if the user is connected as an employee
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      // check if the eye icon exists
+      const eyeIcon = screen.getAllByTestId('icon-eye')[0]
+      // check if the eye icon is clicked
+      userEvent.click(eyeIcon)
+      // ********* check if the icon have been clicked
+      expect(eyeIcon).toHaveBeenClicked()
+      // ********* check if the function handleClickIconEye have been called
+      expect(handleClickIconEye).toHaveBeenCalled()
+      // check if the modal is opened
+      const modal = screen.getByTestId('modaleFile')
+      expect(modal.style.display).toBe('block')
+      // check if the modal contains title
+      expect(modal.querySelector('.content-title').textContent).toBe('Envoyer une note de frais')
+      // check if the modal contains a form
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy()
+    })
+  })
 })
+
