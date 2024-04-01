a49fe6ec8feb920b7f78ccda98a02e342277022d
Cristel <stelda@outlook.fr>
2024-04-01 07:59:30 +0200

    Add test - Bills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -10,19 +10,24 @@ import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 import userEvent from "@testing-library/user-event";
+import * as mockStore from "../fixtures/bills.js";
+import {formatDate, formatStatus} from "../app/format.js";
+
 
 describe("Given I am connected as an employee", () => {
   beforeEach(() => {
-    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    // Set up authentification
+    Object.defineProperty(window, 'localStorage', {value: localStorageMock})
     window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
+      type: 'Employee'
     }))
+    // Set up the document body
     const root = document.createElement("div")
     root.setAttribute("id", "root")
     document.body.append(root)
     router()
     window.onNavigate(ROUTES_PATH.Bills)
-    })
+  })
 
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -32,7 +37,7 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.classList).toContain('active-icon');
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+      document.body.innerHTML = BillsUI({data: bills})
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
@@ -43,9 +48,7 @@ describe("Given I am connected as an employee", () => {
   // test for new bill button
   describe("When I click on New Bill button", () => {
     test("Then it should navigate and display the new bill form", () => {
-      // ********* check if the new bill button exists
       const newBillButton = screen.getByTestId('btn-new-bill')
-      // click on the new bill button
       userEvent.click(newBillButton)
       // check if the window location is new bill page
       expect(window.location.href).toBe('http://localhost/#employee/bill/new')
@@ -58,9 +61,7 @@ describe("Given I am connected as an employee", () => {
   // test for eye icon
   describe("When I click on the eye icon", () => {
     test("Then a modal opens", () => {
-      // check if the eye icon exists
       const eyeIcon = screen.getAllByTestId('icon-eye')[0]
-      // check if the eye icon is clicked
       userEvent.click(eyeIcon)
       // check if the modal is opened
       const modal = screen.getByTestId('modaleFile')
@@ -71,5 +72,116 @@ describe("Given I am connected as an employee", () => {
       expect(screen.getByTestId("form-new-bill")).toBeTruthy()
     })
   })
-})
 
+  // integration test GET
+  describe("When I navigate to Bills", () => {
+    test("then fetches bills from mock API GET", async () => {
+      // check if the bills are fetched from the mock API
+      const bills = await mockStore.bills.list()
+      expect(bills.length).toBeGreaterThan(0)
+    })
+    test("then the bills should be displayed", async () => {
+      // check if the table exists
+      const table = screen.getByTestId("tbody")
+      expect(table).toBeTruthy()
+    })
+
+    // test for date
+    test("then bills should have a correct date", async () => {
+      // check if the bills are fetched from the mock API
+      const bills = await mockStore.bills.list()
+      // check if the date is correct
+      bills.forEach(bill => {
+          const date = bill.date
+          expect(date).toMatch(formatDate)
+      })
+    })
+    test("then it should log an error if date format is incorrect", async () => {
+      // check if the bills are fetched from the mock API
+      const bills = await mockStore.bills.list()
+      // check if the date is incorrect
+      bills[0].date = 'invalid';
+      expect(console.log).toHaveBeenCalledWith('e', 'for', expect.anything());
+    })
+
+    // test for status
+    test("then bills should have a correct status", async () => {
+      // check if the table exists
+      const table = screen.getByTestId("tbody")
+      expect(table).toBeTruthy()
+      // check if the bills are fetched from the mock API
+      const bills = await mockStore.bills.list()
+      // check if the status is correct
+      bills.forEach(bill => {
+        const status = bill.status
+        // expect(status).toMatch(/pending|accepted|refused/)
+        expect(status).toMatch(formatStatus)
+      })
+    })
+    test("then it should log an error if status format is incorrect", async () => {
+      // modify a status to an incorrect format
+        formatStatus.mockImplementation(() => {throw new Error('Invalid status')})
+        const bills = await mockStore.bills.list()
+        bills[0].status = 'invalid'
+        // check if the error is logged
+        expect(console.log).toHaveBeenCalledWith('e', 'for', expect.anything());
+    })
+
+    test("then it should log the length of bills", async () => {
+      const bills = await mockStore.bills.list()
+      // check if the length of bills is logged
+      expect(console.log).toHaveBeenCalledWith('length', bills.length);
+      // check if the length of bills is 4
+      expect(bills.length).toBe(4)
+      const consoleSpy = jest.spyOn(console, 'log');
+      // await getBills();
+      expect(consoleSpy).toHaveBeenCalledWith('length', bills.length);
+    })
+  })
+
+  // test error
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          {value: localStorageMock}
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "e@e"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      router()
+    })
+    test(" then fetches bills from an API and fails with 404 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }
+      })
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }
+      })
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+})
