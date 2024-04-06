7ed53e9f224b338c6e552f128125054bcfaf5222
Cristel <stelda@outlook.fr>
2024-04-06 16:22:41 +0200

    Add test - NewBill when an error occurs

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -152,5 +152,58 @@ describe("Given I am connected as an employee", () => {
 		})
 	})
 
+	// integration test POST error
+	describe("When an error occurs on API", () => {
+		beforeEach(() => {
+			// initialize mockSTore
+			jest.spyOn(mockStore, "bills").mockReturnValue({
+				list: () => Promise.resolve([])
+			});
+
+			Object.defineProperty(
+				window,
+				'localStorage',
+				{value: localStorageMock}
+			)
+			window.localStorage.setItem('user', JSON.stringify({
+				type: 'Employee',
+				email: "e@e"
+			}))
+			const root = document.createElement("div")
+			root.setAttribute("id", "root")
+			document.body.appendChild(root)
+			router()
+		})
+		test("then fetches bills from an API and fails with 404 message error", async () => {
+			mockStore.bills.mockImplementationOnce(() => {
+				return {
+					list: () => {
+						return Promise.reject(new Error("Erreur 404"))
+					}
+				}
+			})
+			window.onNavigate(ROUTES_PATH.Bills)
+			await new Promise(process.nextTick);
+			const message = await screen.getByText(/Erreur/)
+			expect(message).toBeTruthy()
+		})
+		test("then fetches messages from an API and fails with 500 message error", async () => {
+			mockStore.bills.mockImplementationOnce(() => {
+				return {
+					list: () => {
+						return Promise.reject(new Error("Erreur 500"))
+					}
+				}
+			})
+			window.onNavigate(ROUTES_PATH.Bills)
+			await new Promise(process.nextTick);
+			const message = await screen.getByText(/Erreur/)
+			expect(message).toBeTruthy()
+		})
+		afterEach(() => {
+			jest.resetAllMocks()
+		})
+	})
 })
+
 
